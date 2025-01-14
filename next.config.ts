88b953ac198690c4b96ce95e3a57ca6233fb6358
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2025-01-14 18:06:58 -0500

    Add base path

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
+  basePath: "/docs",
   output: 'export',
   images: {
     unoptimized: true // mandatory, otherwise won't export
