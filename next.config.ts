80e5abdccedf8ba14f3a47ebe4897adaa55aed49
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2025-01-14 22:04:49 -0500

    reduce one level of folder for docs

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const withNextra = nextra({
   search: {
     codeblocks: false
   },
-  contentDirBasePath: '/docs'
 });
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
--- src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {}
-
-export default function IndexPage() {
-  return (
-   <>
-   </>
-  )
-}
